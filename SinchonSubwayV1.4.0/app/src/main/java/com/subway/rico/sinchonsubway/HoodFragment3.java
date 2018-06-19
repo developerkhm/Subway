cf5e1f94286c92f22853fe76c81112b113f76d9e
rico <spingferry@gmail.com>
2018-06-19 11:24:36 +0900

    네이버 지도 임시 테스트 소스 추가, 패키지 재정렬 버전

---
+++ SinchonSubwayV1.4.0/app/src/main/java/com/subway/rico/sinchonsubway/HoodFragment3.java
@@ -0,0 +1,190 @@
+package com.subway.rico.sinchonsubway;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.squareup.leakcanary.RefWatcher;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HoodFragment3 extends Fragment {
+    private LinearLayout mBottom;
+    private ImageView mHead, mLineTitle, mImg_Main;
+    public int mImg_res;
+    private ImageView mLine_1, mLine_2, mLine_3, mLine_4, mLine_5, mLine_6, mLine_7, mLine_8, mLine_9, mLine_10;
+    private int mLineRes_1, mLineRes_2, mLineRes_3, mLineRes_4, mLineRes_5, mLineRes_6, mLineRes_7, mLineRes_8, mLineRes_9;
+    private ImageView[] mLine;
+    private int[] mImg_LineRes;
+
+    private int[] mLine_Resource;
+    private int[] mLine_Resource_Press;
+
+    private LineOnClickListener mLineOnClickListener;
+
+    public HoodFragment3() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.hood_fragment3, container, false);
+
+        mHead = (ImageView) view.findViewById(R.id.img_title);
+        mHead.getLayoutParams().width = CommonUtil.getInstance().utilGetViewWidthPx(2160);
+        mHead.getLayoutParams().height = CommonUtil.getInstance().utilGetViewWidthPx(798);
+
+        mLineTitle = (ImageView) view.findViewById(R.id.img_line_title);
+        mLineTitle.getLayoutParams().width = CommonUtil.getInstance().utilGetViewWidthPx(2160);
+        mLineTitle.getLayoutParams().height = CommonUtil.getInstance().utilGetViewWidthPx(164);
+        CommonUtil.getInstance().loadImage(getActivity(), R.drawable.hood_main_3_title, mLineTitle);
+
+        mImg_Main = (ImageView) view.findViewById(R.id.img_main);
+        mImg_Main.getLayoutParams().width = CommonUtil.getInstance().utilGetViewWidthPx(2160);
+        mImg_Main.getLayoutParams().height = CommonUtil.getInstance().utilGetViewWidthPx(1618);
+        PhotoViewAttacher mAttacher = new PhotoViewAttacher(mImg_Main);
+        mAttacher.setScaleType(ImageView.ScaleType.FIT_START);
+
+
+        mBottom = (LinearLayout) view.findViewById(R.id.img_bottom);
+        mBottom.getLayoutParams().width = CommonUtil.getInstance().utilGetViewWidthPx(2160);
+        mBottom.getLayoutParams().height = CommonUtil.getInstance().utilGetViewWidthPx(204 + 50);
+
+        mImg_LineRes = new int[]{R.drawable.hood_main_3_line_1,
+                R.drawable.hood_main_3_line_2,
+                R.drawable.hood_main_3_line_3,
+                R.drawable.hood_main_3_line_4,
+                R.drawable.hood_main_3_line_5,
+                R.drawable.hood_main_3_line_6,
+                R.drawable.hood_main_3_line_7,
+                R.drawable.hood_main_3_line_8,
+                R.drawable.hood_main_3_line_9
+        };
+
+        mLine_1 = (ImageView) view.findViewById(R.id.img_line1);
+        mLine_2 = (ImageView) view.findViewById(R.id.img_line2);
+        mLine_3 = (ImageView) view.findViewById(R.id.img_line3);
+        mLine_4 = (ImageView) view.findViewById(R.id.img_line4);
+        mLine_5 = (ImageView) view.findViewById(R.id.img_line5);
+        mLine_6 = (ImageView) view.findViewById(R.id.img_line6);
+        mLine_7 = (ImageView) view.findViewById(R.id.img_line7);
+        mLine_8 = (ImageView) view.findViewById(R.id.img_line8);
+        mLine_9 = (ImageView) view.findViewById(R.id.img_line9);
+        mLine_10 = (ImageView) view.findViewById(R.id.img_line10);
+
+        mLine = new ImageView[]{mLine_1, mLine_2, mLine_3, mLine_4, mLine_5,
+                mLine_6, mLine_7, mLine_8, mLine_9, mLine_10};
+
+        mLine_Resource = new int[]{
+                R.drawable.hood_main_3_btn_1_dim,
+                R.drawable.hood_main_3_btn_2_dim,
+                R.drawable.hood_main_3_btn_3_dim,
+                R.drawable.hood_main_3_btn_4_dim,
+                R.drawable.hood_main_3_btn_5_dim,
+                R.drawable.hood_main_3_btn_6_dim,
+                R.drawable.hood_main_3_btn_7_dim,
+                R.drawable.hood_main_3_btn_8_dim,
+                R.drawable.hood_main_3_btn_9_dim,
+                R.drawable.hood_main_3_btn_10_blank
+        };
+
+        mLine_Resource_Press = new int[]{
+                R.drawable.hood_main_3_btn_1,
+                R.drawable.hood_main_3_btn_2,
+                R.drawable.hood_main_3_btn_3,
+                R.drawable.hood_main_3_btn_4,
+                R.drawable.hood_main_3_btn_5,
+                R.drawable.hood_main_3_btn_6,
+                R.drawable.hood_main_3_btn_7,
+                R.drawable.hood_main_3_btn_8,
+                R.drawable.hood_main_3_btn_9,
+                R.drawable.hood_main_3_btn_10_blank
+        };
+
+        mLineOnClickListener = new LineOnClickListener();
+        for (int i = 0; i < mLine.length; i++) {
+            mLine[i].setOnClickListener(mLineOnClickListener);
+        }
+        mImg_res = mImg_LineRes[1]; //2호선
+        CommonUtil.getInstance().loadImage(getActivity(), mImg_res, mImg_Main);
+
+
+        btnReset();
+        CommonUtil.getInstance().loadImage(getActivity(), mLine_Resource_Press[1], mLine[1]);
+
+        return view;
+    }
+
+    class LineOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            btnReset();
+            int imgRes = 0;
+            int id = 0;
+            switch (v.getId()) {
+                case R.id.img_line1:
+                    imgRes = mImg_LineRes[0];
+                    id = 0;
+                    break;
+                case R.id.img_line2:
+                    imgRes = mImg_LineRes[1];
+                    id = 1;
+                    break;
+                case R.id.img_line3:
+                    imgRes = mImg_LineRes[2];
+                    id = 2;
+                    break;
+                case R.id.img_line4:
+                    imgRes = mImg_LineRes[3];
+                    id = 3;
+                    break;
+                case R.id.img_line5:
+                    imgRes = mImg_LineRes[4];
+                    id = 4;
+                    break;
+                case R.id.img_line6:
+                    imgRes = mImg_LineRes[5];
+                    id = 5;
+                    break;
+                case R.id.img_line7:
+                    imgRes = mImg_LineRes[6];
+                    id = 6;
+                    break;
+                case R.id.img_line8:
+                    imgRes = mImg_LineRes[7];
+                    id = 7;
+                    break;
+                case R.id.img_line9:
+                    imgRes = mImg_LineRes[8];
+                    id = 8;
+                    break;
+
+            }
+            CommonUtil.getInstance().loadImage(getActivity(), mLine_Resource_Press[id], mLine[id]);
+//            mLine[id].setBackgroundResource(mLine_Resource_Press[id]);
+            CommonUtil.getInstance().loadImage(getContext(), imgRes, mImg_Main);
+        }
+    }
+
+    private void btnReset() {
+        for (int i = 0; i < mLine_Resource.length; i++) {
+//            mLine[i].setBackgroundResource(mLine_Resource[i]);
+            CommonUtil.getInstance().loadImage(getActivity(), mLine_Resource[i], mLine[i]);
+        }
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        RefWatcher refWatcher = LeakCanaryApplication.getRefWatcher(getActivity());
+        refWatcher.watch(this);
+    }
+}
---
+++ SinchonSubwayV1.4.0/app/src/main/java/com/subway/rico/sinchonsubway/MainActivity.java
@@ -0,0 +1,191 @@
+package com.subway.rico.sinchonsubway;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import org.apache.log4j.Logger;
+
+import de.mindpipe.android.logging.log4j.LogConfigurator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_WRITE_STORAGE_REQUEST_CODE = 112;
+    private boolean isAuto = true;
+    private LogConfigurator logConfigurator;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        requestAppPermissions();
+
+        logConfigurator = new LogConfigurator();
+        logConfigurator.setFileName(Environment.getExternalStorageDirectory() + "/Subway/Logs/logFile.log");
+        logConfigurator.configure();
+
+        initStart();
+    }
+
+    private void initStart() {
+        Logger logger = Logger.getLogger(MainActivity.class.getSimpleName());
+        CommonUtil.getInstance().MainActivity = MainActivity.this;
+
+        Button btn1 = (Button) findViewById(R.id.st_button1);
+        Button btn2 = (Button) findViewById(R.id.st_button2);
+        Button btn3 = (Button) findViewById(R.id.st_button3);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAuto = false;
+                savePreferences(1);
+
+                Intent intent = new Intent(MainActivity.this, StationActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                startActivity(new Intent(MainActivity.this, StationActivity.class));
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAuto = false;
+                savePreferences(2);
+
+                Intent intent = new Intent(MainActivity.this, HoodActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                startActivity(intent);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAuto = false;
+                savePreferences(3);
+
+                Intent intent = new Intent(MainActivity.this, ExitActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+            }
+        });
+
+        btn1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!isAuto)
+                    return;
+
+                switch (getPreferences()) {
+                    case 1:
+                        Intent intent = new Intent(MainActivity.this, StationActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        Intent intent2 = new Intent(MainActivity.this, HoodActivity.class);
+                        intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                        startActivity(intent2);
+                        break;
+                    case 3:
+                        Intent intent3 = new Intent(MainActivity.this, ExitActivity.class);
+                        intent3.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        startActivity(intent3);
+                        break;
+                    default:
+                }
+            }
+        }, 4000);
+    }
+
+    // 값 불러오기
+    private int getPreferences() {
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        return pref.getInt("state", 1);
+    }
+
+    // 값 저장하기
+    private void savePreferences(int state) {
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("state", state);
+        editor.commit();
+    }
+
+    // 값(Key Data) 삭제하기
+    private void removePreferences() {
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.remove("hi");
+        editor.commit();
+    }
+
+    // 값(ALL Data) 삭제하기
+    private void removeAllPreferences() {
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+    private void requestAppPermissions() {
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return;
+        }
+
+        if (hasReadPermissions() && hasWritePermissions()) {
+            return;
+        }
+
+        ActivityCompat.requestPermissions(this,
+                new String[] {
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                }, REQUEST_WRITE_STORAGE_REQUEST_CODE); // your request code
+    }
+
+    private boolean hasReadPermissions() {
+        return (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private boolean hasWritePermissions() {
+        return (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+    }
+
+}
