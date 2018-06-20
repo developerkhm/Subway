53c34a1ad8ace9b389f8021e42bd39870b82112e
rico <spingferry@gmail.com>
2018-06-20 18:19:18 +0900

    신촌역 수정

--- SinchonSubwayV1.4.0/app/src/main/java/com/subway/rico/sinchonsubway/MainActivity.java
+++ SinchonSubwayV1.4.0/app/src/main/java/com/subway/rico/sinchonsubway/MainActivity.java
@@ -7,11 +7,18 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.subway.rico.sinchonsubway.common.CommonUtil;
+import com.subway.rico.sinchonsubway.exit.ExitActivity;
+import com.subway.rico.sinchonsubway.hood.HoodActivity;
+import com.subway.rico.sinchonsubway.station.StationActivity;
 
 import org.apache.log4j.Logger;
 
@@ -22,24 +29,18 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_WRITE_STORAGE_REQUEST_CODE = 112;
     private boolean isAuto = true;
     private LogConfigurator logConfigurator;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onStart() {
         super.onStart();
-        initStart();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-
-        requestAppPermissions();
-
-        logConfigurator = new LogConfigurator();
-        logConfigurator.setFileName(Environment.getExternalStorageDirectory() + "/Subway/Logs/logFile.log");
-        logConfigurator.configure();
-
+        setContentView(com.subway.rico.sinchonsubway.R.layout.main_activity);
+        checkPermission();
         initStart();
     }
 
@@ -47,9 +48,9 @@ public class MainActivity extends AppCompatActivity {
         Logger logger = Logger.getLogger(MainActivity.class.getSimpleName());
         CommonUtil.getInstance().MainActivity = MainActivity.this;
 
-        Button btn1 = (Button) findViewById(R.id.st_button1);
-        Button btn2 = (Button) findViewById(R.id.st_button2);
-        Button btn3 = (Button) findViewById(R.id.st_button3);
+        Button btn1 = (Button) findViewById(com.subway.rico.sinchonsubway.R.id.st_button1);
+        Button btn2 = (Button) findViewById(com.subway.rico.sinchonsubway.R.id.st_button2);
+        Button btn3 = (Button) findViewById(com.subway.rico.sinchonsubway.R.id.st_button3);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -92,41 +93,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        btn1.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (!isAuto)
-                    return;
-
-                switch (getPreferences()) {
-                    case 1:
-                        Intent intent = new Intent(MainActivity.this, StationActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
-                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-                        startActivity(intent);
-                        break;
-                    case 2:
-                        Intent intent2 = new Intent(MainActivity.this, HoodActivity.class);
-                        intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
-                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-                        startActivity(intent2);
-                        break;
-                    case 3:
-                        Intent intent3 = new Intent(MainActivity.this, ExitActivity.class);
-                        intent3.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
-                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                        startActivity(intent3);
-                        break;
-                    default:
-                }
-            }
-        }, 4000);
     }
 
     // 값 불러오기
@@ -164,28 +130,135 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void requestAppPermissions() {
-        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            return;
+//    private void requestAppPermissions() {
+//        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+//            return;
+//        }
+//
+//        if (hasReadPermissions() && hasWritePermissions()) {
+//            return;
+//        }
+//
+//        ActivityCompat.requestPermissions(this,
+//                new String[] {
+//                        Manifest.permission.READ_EXTERNAL_STORAGE,
+//                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+//                }, REQUEST_WRITE_STORAGE_REQUEST_CODE); // your request code
+//    }
+//
+//    private boolean hasReadPermissions() {
+//        return (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+//    }
+//
+//    private boolean hasWritePermissions() {
+//        return (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+//    }
+
+    /**
+     * Permission check.
+     */
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED
+                    || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    // Explain to the user why we need to write the permission.
+                    Toast.makeText(this, "Read/Write external storage", Toast.LENGTH_SHORT).show();
+                }
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{
+                                Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        }, REQUEST_WRITE_STORAGE_REQUEST_CODE); // your request code
+
+//                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+//                        REQUEST_WRITE_STORAGE_REQUEST_CODE);
+
+                // MY_PERMISSION_REQUEST_STORAGE is an
+                // app-defined int constant
+
+            } else {
+                // 다음 부분은 항상 허용일 경우에 해당이 됩니다.
+                initLogwite();
+                autoStart();
+            }
+        } else {
+            initLogwite();
+            autoStart();
         }
+    }
 
-        if (hasReadPermissions() && hasWritePermissions()) {
-            return;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_WRITE_STORAGE_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    initLogwite();
+                    autoStart();
+
+                    // permission was granted, yay! do the
+                    // calendar task you need to do.
+
+                } else {
+                    int pid = android.os.Process.myPid();
+                    android.os.Process.killProcess(pid);
+
+//                    Log.d(TAG, "Permission always deny");
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
         }
-
-        ActivityCompat.requestPermissions(this,
-                new String[] {
-                        Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-                }, REQUEST_WRITE_STORAGE_REQUEST_CODE); // your request code
     }
 
-    private boolean hasReadPermissions() {
-        return (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+    private void initLogwite() {
+        logConfigurator = new LogConfigurator();
+        logConfigurator.setFileName(Environment.getExternalStorageDirectory() + "/Subway/Logs/logFile.log");
+        logConfigurator.configure();
     }
 
-    private boolean hasWritePermissions() {
-        return (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
-    }
+    private void autoStart(){
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!isAuto)
+                    return;
 
+                switch (getPreferences()) {
+                    case 1:
+                        Intent intent = new Intent(MainActivity.this, StationActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        Intent intent2 = new Intent(MainActivity.this, HoodActivity.class);
+                        intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                        startActivity(intent2);
+                        break;
+                    case 3:
+                        Intent intent3 = new Intent(MainActivity.this, ExitActivity.class);
+                        intent3.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        startActivity(intent3);
+                        break;
+                    default:
+                }
+            }
+        }, 4000);
+    }
 }
--- SubwaySinChonV1.3.0/app/src/main/java/com/ricointeractive/main/MainActivity.java
+++ SubwaySinChonV1.3.0/app/src/main/java/com/ricointeractive/main/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
 
         logConfigurator = new LogConfigurator();
         logConfigurator.setFileName(Environment.getExternalStorageDirectory() + "/Subway/Logs/logFile.log");
