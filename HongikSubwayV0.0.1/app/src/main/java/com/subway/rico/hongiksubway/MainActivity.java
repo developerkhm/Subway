bb1ec98505f784d7b6780e417bcce25cfdcb435b
rico <spingferry@gmail.com>
2018-06-20 18:21:48 +0900

    홍익 프로젝트 추가

---
+++ HongikSubwayV0.0.1/app/src/main/java/com/subway/rico/hongiksubway/MainActivity.java
@@ -0,0 +1,265 @@
+package com.subway.rico.hongiksubway;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.subway.rico.hongiksubway.common.CommonUtil;
+import com.subway.rico.hongiksubway.exit.ExitActivity;
+import com.subway.rico.hongiksubway.hood.HoodActivity;
+import com.subway.rico.hongiksubway.station.StationActivity;
+
+import org.apache.log4j.Logger;
+
+import de.mindpipe.android.logging.log4j.LogConfigurator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_WRITE_STORAGE_REQUEST_CODE = 112;
+    private boolean isAuto = true;
+    private LogConfigurator logConfigurator;
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.subway.rico.hongiksubway.R.layout.main_activity);
+        checkPermission();
+        initStart();
+    }
+
+    private void initStart() {
+        Logger logger = Logger.getLogger(MainActivity.class.getSimpleName());
+        CommonUtil.getInstance().MainActivity = MainActivity.this;
+
+        Button btn1 = (Button) findViewById(com.subway.rico.hongiksubway.R.id.st_button1);
+        Button btn2 = (Button) findViewById(com.subway.rico.hongiksubway.R.id.st_button2);
+        Button btn3 = (Button) findViewById(com.subway.rico.hongiksubway.R.id.st_button3);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAuto = false;
+                savePreferences(1);
+
+                Intent intent = new Intent(MainActivity.this, StationActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                startActivity(new Intent(MainActivity.this, StationActivity.class));
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAuto = false;
+                savePreferences(2);
+
+                Intent intent = new Intent(MainActivity.this, HoodActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                startActivity(intent);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isAuto = false;
+                savePreferences(3);
+
+                Intent intent = new Intent(MainActivity.this, ExitActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    // 값 불러오기
+    private int getPreferences() {
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        return pref.getInt("state", 1);
+    }
+
+    // 값 저장하기
+    private void savePreferences(int state) {
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("state", state);
+        editor.commit();
+    }
+
+    // 값(Key Data) 삭제하기
+    private void removePreferences() {
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.remove("hi");
+        editor.commit();
+    }
+
+    // 값(ALL Data) 삭제하기
+    private void removeAllPreferences() {
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+//    private void requestAppPermissions() {
+//        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+//            return;
+//        }
+//
+//        if (hasReadPermissions() && hasWritePermissions()) {
+//            return;
+//        }
+//
+//        ActivityCompat.requestPermissions(this,
+//                new String[] {
+//                        Manifest.permission.READ_EXTERNAL_STORAGE,
+//                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+//                }, REQUEST_WRITE_STORAGE_REQUEST_CODE); // your request code
+//    }
+//
+//    private boolean hasReadPermissions() {
+//        return (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+//    }
+//
+//    private boolean hasWritePermissions() {
+//        return (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+//    }
+
+    /**
+     * Permission check.
+     */
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED
+                    || checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    // Explain to the user why we need to write the permission.
+                    Toast.makeText(this, "Read/Write external storage", Toast.LENGTH_SHORT).show();
+                }
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{
+                                Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        }, REQUEST_WRITE_STORAGE_REQUEST_CODE); // your request code
+
+//                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+//                        REQUEST_WRITE_STORAGE_REQUEST_CODE);
+
+                // MY_PERMISSION_REQUEST_STORAGE is an
+                // app-defined int constant
+
+            } else {
+                // 다음 부분은 항상 허용일 경우에 해당이 됩니다.
+                initLogwite();
+                autoStart();
+            }
+        } else {
+            initLogwite();
+            autoStart();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_WRITE_STORAGE_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    initLogwite();
+                    autoStart();
+
+                    // permission was granted, yay! do the
+                    // calendar task you need to do.
+
+                } else {
+                    int pid = android.os.Process.myPid();
+                    android.os.Process.killProcess(pid);
+
+//                    Log.d(TAG, "Permission always deny");
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+        }
+    }
+
+    private void initLogwite() {
+        logConfigurator = new LogConfigurator();
+        logConfigurator.setFileName(Environment.getExternalStorageDirectory() + "/Subway/Logs/logFile.log");
+        logConfigurator.configure();
+    }
+
+    private void autoStart(){
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!isAuto)
+                    return;
+
+                switch (getPreferences()) {
+                    case 1:
+                        Intent intent = new Intent(MainActivity.this, StationActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        Intent intent2 = new Intent(MainActivity.this, HoodActivity.class);
+                        intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                        startActivity(intent2);
+                        break;
+                    case 3:
+                        Intent intent3 = new Intent(MainActivity.this, ExitActivity.class);
+                        intent3.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        startActivity(intent3);
+                        break;
+                    default:
+                }
+            }
+        }, 4000);
+    }
+}
---
+++ HongikSubwayV0.0.1/app/src/main/java/com/subway/rico/hongiksubway/exit/ExitFragment4.java
@@ -0,0 +1,158 @@
+package com.subway.rico.hongiksubway.exit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.subway.rico.hongiksubway.common.CommonUtil;
+import com.subway.rico.hongiksubway.R;
+
+import java.util.ArrayList;
+
+public class ExitFragment4 extends Fragment {
+    private ImageView mImg_Title, mImg_Main;
+    private int mImg_res;
+
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private RecyclerViewAdapter mAdapter;
+
+
+    private final int MAX_TITLE_IMG_W = 2160;
+    private final int MAX_TITLEIMG_H = 798;
+    private final int MAX_MAIN_W = 2160;
+    private final int MAX_MAIN_H = 1146;
+    private final int MAX_RECYLER_W = 2160;
+    private final int MAX_RECYLER_H = 0;
+
+    public ExitFragment4() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.exit_fragment4, container, false);
+
+        mImg_Title = (ImageView) view.findViewById(R.id.img_title);
+        mImg_Title.getLayoutParams().width = CommonUtil.getInstance().utilGetViewWidthPx(MAX_TITLE_IMG_W);
+        mImg_Title.getLayoutParams().height = CommonUtil.getInstance().utilGetViewWidthPx(MAX_TITLEIMG_H);
+        CommonUtil.getInstance().loadImage(getActivity(), R.drawable.exit_head, mImg_Title);
+
+        mImg_res = R.drawable.exit_main_3;
+        mImg_Main = (ImageView) view.findViewById(R.id.img_main);
+        mImg_Main.getLayoutParams().width = CommonUtil.getInstance().utilGetViewWidthPx(MAX_MAIN_W);
+        mImg_Main.getLayoutParams().height = CommonUtil.getInstance().utilGetViewWidthPx(MAX_MAIN_H);
+        CommonUtil.getInstance().loadImage(getActivity(), mImg_res, mImg_Main);
+
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mRecyclerView.getLayoutParams().width = CommonUtil.getInstance().utilGetViewWidthPx(MAX_RECYLER_W);
+        //mRecyclerView.getLayoutParams().height = utilGetViewWidthPx((3840-1146));
+
+        setRecycle();
+
+
+        return view;
+    }
+
+    public class Item {
+        public int res;
+
+        public int getRes() {
+            return res;
+        }
+
+        public Item(int res) {
+            this.res = res;
+        }
+    }
+
+    private void setRecycle() {
+        mLayoutManager = new LinearLayoutManager(getContext());
+        //mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        // ArrayList 에 Item 객체(데이터) 넣기
+        ArrayList<Item> items = new ArrayList();
+
+        items.add(new Item(R.drawable.exit_main_4_banner_sample));
+        items.add(new Item(R.drawable.exit_main_4_banner_sample_2));
+        items.add(new Item(R.drawable.exit_main_4_banner_sample_3));
+        items.add(new Item(R.drawable.exit_main_4_banner_sample));
+
+        mRecyclerView.setLayoutManager(mLayoutManager); // LinearLayout으로 설정
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator()); // Animation Defualt 설정
+        // mRecyclerView.addItemDecoration(new RecyclerViewDecoration(this, RecyclerViewDecoration.VERTICAL_LIST)); //Decoration 설정
+        mAdapter = new RecyclerViewAdapter(items);
+        mRecyclerView.setAdapter(mAdapter);
+        //mAdapter.notifyDataSetChanged();
+    }
+
+    public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder> {
+
+        private ArrayList<Item> mItems;
+        Context mContext;
+
+        public RecyclerViewAdapter(ArrayList itemList) {
+            mItems = itemList;
+        }
+
+        // 필수 오버라이드 : View 생성, ViewHolder 호출
+        @Override
+
+        public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.exit_main_4_recycle_item, parent, false);
+            mContext = parent.getContext();
+            RecyclerViewHolder holder = new RecyclerViewHolder(v);
+            return holder;
+        }
+
+        // 필수 오버라이드 : 재활용되는 View 가 호출, Adapter 가 해당 position 에 해당하는 데이터를 결합
+        @Override
+        public void onBindViewHolder(RecyclerViewHolder holder, final int position) {
+            // 해당 position 에 해당하는 데이터 결합
+//            holder.mIndex.setText(mItems.get(position).index);
+            CommonUtil.getInstance().loadImage(mContext, mItems.get(position).res, holder.mRes);
+//            holder.mName.setText(mItems.get(position).name);
+            Log.d("test", "onBindViewHolder : 호출 호출 호출");
+
+            // 이벤트처리 : 생성된 List 중 선택된 목록번호를 Toast로 출력
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    Toast.makeText(mContext, String.format("%d 선택", position + 1), Toast.LENGTH_SHORT).show();
+                    CommonUtil.getInstance().callToast((Activity) mContext);
+                }
+            });
+        }
+
+        // 필수 오버라이드 : 데이터 갯수 반환
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
+    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mRes;
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            mRes = (ImageView) itemView.findViewById(R.id.index);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
