cbd882e7550865577685d025f489d78a129d4567
DESKTOP-4HFLJ04\k <spingferry@gmail.com>
2018-08-16 21:40:23 +0900

    수정

---
+++ Table/syruptable/app/src/main/java/com/skt/tmaphot/net/service/APIClient.java
@@ -0,0 +1,26 @@
+package com.skt.tmaphot.net.service;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.ordertable.co.kr/v1/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+}
